caf77a65bb9ffa98237c95697ec3ff011013ffe7
Ilona Shcherbakova <shcherbakova0911@mail.ru>
2019-09-22 09:31:36 +0300

    Initial Commit

---
+++ HelloWorld!3/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HelloWorld!3
+//
+//  Created by Ilona Shcherbakova on 22/09/2019.
+//  Copyright © 2019 Ilona Shcherbakova. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
